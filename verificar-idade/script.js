87a81b7afae518a017bb8d2de91361749c2a4cb4
dudaborges <duda.pborges92@gmail.com>
2022-09-04 13:52:32 -0300

    update

---
+++ verificar-idade/script.js
@@ -0,0 +1,18 @@
+function Verificar(){
+    //criar as variáveis, para dar nome aos valores
+    var anoNasc = document.getElementById('txtano')
+    var mulher = document.getElementById('fem')
+    var homem = document.getElementById('masc')
+
+    //para calcular o ano de nascimento, precisamos pegar o ano atual
+    var diaHoje = new Date()
+    var anoAtual = diaHoje.getFullYear
+
+    //antes de calcularmos, precisamos preparar o sistema caso alguém não preencha algo, ou coloque uma data acima do ano atual
+
+    if (anoNasc.value.leght){
+        
+    }
+
+
+}
