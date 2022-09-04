8804892a82b00e2a94205046e734a1e657abe433
dudaborges <duda.pborges92@gmail.com>
2022-09-04 14:03:24 -0300

    update

--- length/script.js
+++ length/script.js
@@ -11,3 +11,15 @@ frutas = ["Banana", " Maça", " Laranja", " Melancia"]
 var resultado = frutas.length
 
 console.log(`A lista de frutas com ${frutas}, possui ${resultado} frutas!`)
+
+//na estrutura de repetição
+
+produtos = ["caneta", "borracha", "apontador", "régua", "lápis"]
+qntProdutos = produtos.length
+
+//valor iniciado, critério de parada e incremento
+//vai repetir no máximo até a quantidade de produtos
+console.log('Lista de produtos: ')
+for (var repetir = 0; repetir < qntProdutos; repetir++){
+    console.log(produtos[repetir])
+}
