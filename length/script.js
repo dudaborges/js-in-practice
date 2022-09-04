ed45394f4c0ae6d8cad95a5fe2b851df1b996454
dudaborges <duda.pborges92@gmail.com>
2022-09-04 13:36:52 -0300

    add

---
+++ length/script.js
@@ -0,0 +1,6 @@
+//A propriedade "length" mostra o tamanho de uma string ou o tamanho de uma array
+
+var texto = "Aprendedo length no JavaScript"
+var tamanho = texto.length
+
+console.log(`O texto "${texto}", tem ${tamanho} dígitos!`)
