1346ff0b859718d3bd5042705732afa99194d4a6
dudaborges <duda.pborges92@gmail.com>
2022-09-04 19:16:31 -0300

    update

---
+++ testes/script.js
@@ -0,0 +1,17 @@
+function somar(){
+    var n1 = document.getElementById('n1')
+    var n2 = document.getElementById('n2')
+    var soma = n1.value + n2.value
+    var res = document.getElementById('res')
+
+    res.innerHTML = soma
+
+}
+
+// var hoje = new Date()
+// var anoAtual = hoje.getFullYear()
+// var n = '2'
+// var soma = anoAtual - n
+// console.log(soma)
+
+//subtração não gera concatenação
