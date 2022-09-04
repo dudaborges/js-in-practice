437ea6c641f0706238f2da00d259969ef88c69e4
dudaborges <duda.pborges92@gmail.com>
2022-09-04 18:10:03 -0300

    update

---
+++ contagem/script.js
@@ -0,0 +1,21 @@
+function contar(){
+    var tinicio = document.getElementById('txtinicio')
+    var tfim = document.getElementById('txtfim')
+    var tpasso = document.getElementById('txtpasso')
+    var res = document.getElementById('res')
+
+    var inicio = Number(tinicio)
+    var fim = Number(tfim)
+    var passo = Number(tpasso)
+
+    if(inicio == 0 || fim == 0){
+        res.innerText = 'Impossível contar!'
+    }else if(passo == 0){
+        alert('Passo inválido! Considerando PASSO 1')
+        for(inicio = 1; inicio >= fim; passo){
+            
+        }
+    }
+
+}
+
