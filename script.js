1fff3acb5f35ef5e0bcd88239f48324b54719e72
dudaborges <duda.pborges92@gmail.com>
2022-09-04 19:08:54 -0300

    update

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-function somar(){
-    var n1 = document.getElementById('n1')
-    var n2 = document.getElementById('n2')
-    var soma = n1.value + n2.value
-    var res = document.getElementById('res')
+// function somar(){
+//     var n1 = document.getElementById('n1')
+//     var n2 = document.getElementById('n2')
+//     var soma = Number(n1.value) + Number(n2.value)
+//     var res = document.getElementById('res')
 
-    res.innerHTML = soma
+//     res.innerHTML = soma
 
-}
+// }
+
+var hoje = new Date()
+var anoAtual = hoje.getFullYear()
+var n = '2'
+var soma = anoAtual - n
+console.log(soma)
+//subtração não gera concatenação
