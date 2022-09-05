c74f8de1c4dc6af9d489e12c6b77d07c96541d43
dudaborges <duda.pborges92@gmail.com>
2022-09-05 19:20:46 -0300

    update

--- contagem/script.js
+++ contagem/script.js
@@ -8,14 +8,16 @@ function contar(){
         alert('[ERRO] Faltam dados!')
     }else{
         res.innerHTML = 'Contando: '
-        var inicio = Number(tinicio)
-        var fim = Number(tfim)
-        var passo = Number(tpasso)
+        var inicio = Number(tinicio.value)
+        var fim = Number(tfim.value)
+        var passo = Number(tpasso.value)
+        //Sempre quando lidar tratamento de dados vindo de inputs, é preciso colocar .value para pegar o valor de dentro da caixinha.
+
 
         //o contador começa no número do início, o contador tem uma condição que não pode ultrapassar o fim, e o incremento no contador é adicionar o passo
-        for(var contador = inicio; contador <= fim; contador += p){
+        for(var contador = inicio; contador <= fim; contador += passo){
             //+= é o que ele já tem mais alguma coisa
-            res.innerHTML += `${c}`
+            res.innerHTML += `${contador}`
         }
     }
 }
