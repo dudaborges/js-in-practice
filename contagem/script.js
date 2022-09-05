20d81f65f4ca1680d5afce2c124fdcc4326afa71
dudaborges <duda.pborges92@gmail.com>
2022-09-05 17:34:53 -0300

    update

--- contagem/script.js
+++ contagem/script.js
@@ -7,7 +7,15 @@ function contar(){
     if(tinicio.value.length == 0 || tfim.value.length == 0 || tpasso.value.length == 0){
         alert('[ERRO] Faltam dados!')
     }else{
-        alert('ok')
+        res.innerHTML = 'Contando: '
+        var inicio = Number(tinicio)
+        var fim = Number(tfim)
+        var passo = Number(tpasso)
+
+        //o contador começa no número do início, o contador tem uma condição que não pode ultrapassar o fim, e o incremento no contador é adicionar o passo
+        for(var contador = inicio; contador <= fim; contador += p){
+
+        }
     }
 }
 
