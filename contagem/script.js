72de2880c7bb12a405302666829520bfa1a8fb42
dudaborges <duda.pborges92@gmail.com>
2022-09-05 19:44:57 -0300

    update

--- contagem/script.js
+++ contagem/script.js
@@ -1,30 +1,30 @@
 function contar(){
-    var tinicio = document.getElementById('txtinicio')
-    var tfim = document.getElementById('txtfim')
-    var tpasso = document.getElementById('txtpasso')
-    var res = document.getElementById('res')
+    let tinicio = document.getElementById('txtinicio')
+    let tfim = document.getElementById('txtfim')
+    let tpasso = document.getElementById('txtpasso')
+    let res = document.getElementById('res')
 
     if(tinicio.value.length == 0 || tfim.value.length == 0 || tpasso.value.length == 0){
         alert('[ERRO] Faltam dados!')
         res.innerHTML = 'Impossível contar!'
     }else{
         res.innerHTML = 'Contando: '
-        var inicio = Number(tinicio.value)
-        var fim = Number(tfim.value)
-        var passo = Number(tpasso.value)
+        let inicio = Number(tinicio.value)
+        let fim = Number(tfim.value)
+        let passo = Number(tpasso.value)
         //Sempre quando lidar tratamento de dados vindo de inputs, é preciso colocar .value para pegar o valor de dentro da caixinha.
 
         if(inicio < fim){
         
         //o contador começa no número do início, o contador tem uma condição que não pode ultrapassar o fim, e o incremento no contador é adicionar o passo
-        for(var contador = inicio; contador <= fim; contador += passo){
+        for(let contador = inicio; contador <= fim; contador += passo){
             //+= é o que ele já tem mais alguma coisa
             res.innerHTML += `${contador} \u{1F449}`
         }
         
         }else{
             //fazer do menor pro maior
-            for(var contador = inicio; contador >= fim; contador -= passo){
+            for(let contador = inicio; contador >= fim; contador -= passo){
                 res.innerHTML += `${contador} \u{1F449}`
             }
         }
