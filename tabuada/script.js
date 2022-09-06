1e1d2cab3ec789085245ecf46b4878fe2a41801b
dudaborges <duda.pborges92@gmail.com>
2022-09-06 19:39:48 -0300

    update

--- tabuada/script.js
+++ tabuada/script.js
@@ -7,5 +7,17 @@ function tabuada(){
     }else{
         let num = Number(n.value)
 
+        tab.innerHTML = '' //para limpar a área de tabuada antes de calcular um novo número
+        for(let c = 1; c <= 10; c++){
+            let conta = c * num
+            let item = document.createElement('option')
+            item.text = `${num} x ${c} = ${conta}`
+            tab.appendChild(item)
+        }
     }
 }
+
+//let item = document.createElement('option')
+//item.text = `isso * isso = da isso`
+//tab.appendChild(item)
+//appendChile significa adicionar um elemento filho
