eb34f717e967ccbadc2ee5d74b44efcfbfbff09e
dudaborges <duda.pborges92@gmail.com>
2022-09-06 18:43:10 -0300

    update

---
+++ testes/script.js
@@ -0,0 +1,23 @@
+var n1 = 2
+
+
+// for (var tabuada = n1; tabuada <= n1 * 10; n1 *= )
+
+// var c = 1
+
+//  while (c <= 6) {
+//     console.log('Olá')
+//     c++ 
+//  }
+
+
+// do {
+//     console.log('Olá')
+//     c++ 
+//  } while (c <= 6)
+
+
+while (tabuada <= n1 * 10){
+    console.log(n1)
+    n1 * 1
+}
