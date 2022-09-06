ca2168b0382b8dd50c72231696e96237e946695f
dudaborges <duda.pborges92@gmail.com>
2022-09-06 19:17:11 -0300

    update

---
+++ testes/tabuada.js
@@ -0,0 +1,17 @@
+// var n = 2
+
+// for (var contador = 1; contador <= 10; contador = contador * n){
+//     console.log(contador)
+//     contador++
+// }
+
+// let c = 1
+
+// while (c <= 10){
+//     let num = 2
+//     let conta = num * c
+//     c++
+//     console.log(conta)
+// }
+
+
