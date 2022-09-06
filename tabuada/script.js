3b9ca35c6963a5c664c61c54ee0acecd2106da49
dudaborges <duda.pborges92@gmail.com>
2022-09-06 18:49:23 -0300

    update

---
+++ tabuada/script.js
@@ -0,0 +1,11 @@
+function tabuada(){
+    let n = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+
+    if (n.value.length == 0){
+        alert('Por favor, digite um número!')
+    }else{
+        let num = Number(n.value)
+
+    }
+}
