ed30074e54720bb04d7d8d0f57e5963d659ecbe6
dudaborges <duda.pborges92@gmail.com>
2022-09-09 11:26:02 -0300

    addJs

---
+++ link-botao/script.js
@@ -0,0 +1,9 @@
+
+function link(){
+    let res = document.getElementById('res')
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+    img.setAttribute('src', 'figma-epraja.PNG')
+    res.appendChild(img)
+}
+
