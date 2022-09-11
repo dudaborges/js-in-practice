f470375771c943c509e48d80f973e8b75115ae14
dudaborges <duda.pborges92@gmail.com>
2022-09-11 20:25:15 -0300

    update

--- array/script.js
+++ array/script.js
@@ -11,7 +11,12 @@
 // imprima o seguinte formato de texto com todos os números:
 // "O número x está na posição y"
 
+console.log("Questão 1 - Criar um array com os seguintes números: 10, 4, 7, 12")
 let num = [10, 4, 7, 12]
+console.log(num)
 
+console.log("Questão 2 - Em que posição está o 4?")
 console.log(`O número 4 está na posição ${num.indexOf(4)}`)
+
+
 
