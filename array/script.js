42ebe680f1f1e436700e52cf59dcb2228597615b
dudaborges <duda.pborges92@gmail.com>
2022-09-11 20:18:55 -0300

    addJs

---
+++ array/script.js
@@ -0,0 +1,13 @@
+// criar um array com os seguintes números: 10, 4, 7, 12
+
+// em que posição está o 4?
+
+// tem quantos ítens na lista?
+
+// substitua o 7 pelo 8
+
+// adicione o número 1 na lista
+
+// imprima o seguinte formato de texto com todos os números:
+// "O número x está na posição y"
+
