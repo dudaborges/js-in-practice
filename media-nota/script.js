a5e641f3df15c1ab6269589fc70994e1f14e96bf
dudaborges <duda.pborges92@gmail.com>
2022-09-21 17:13:37 -0300

    update

--- media-nota/script.js
+++ media-nota/script.js
@@ -1,4 +1,12 @@
-let n1 = document.getElementById('txtn1') 
-let n2 = document.getElementById('txtn2')
-let n3 = document.getElementById('txtn3')
+let num1 = document.getElementById('txtn1') 
+let num2 = document.getElementById('txtn2')
+let num3 = document.getElementById('txtn3')
 let res = document.getElementById('res')
+
+function gerar(){
+    let n1 = Number(num1.value)
+    let n2 = Number(num2.value)
+    let n3 = Number(num3.value)
+    let media = (n1 + n2 + n3) / 3
+    res.innerText = `A sua média deu ${media}`
+}
