b2f62104302f68e2e97fb85b3fb9eac16c5deec7
dudaborges <duda.pborges92@gmail.com>
2022-09-21 17:21:14 -0300

    update

--- media-nota/script.js
+++ media-nota/script.js
@@ -7,6 +7,19 @@ function gerar(){
     let n1 = Number(num1.value)
     let n2 = Number(num2.value)
     let n3 = Number(num3.value)
-    let media = (n1 + n2 + n3) / 3
-    res.innerText = `A sua média deu ${media}`
+
+    if (n1 < 1 || n1 > 10 || n2 < 1 || n2 > 10 || n3 < 1 || n3 > 10 ){
+        alert('Preencha as notas entre 1 a 10!')
+    }else{
+        let media = (n1 + n2 + n3) / 3
+        res.innerText = `A sua média deu ${media}`
+        if (media >= 7){
+            res.innerHTML += '<br> aprovado!'
+        }else{
+            res.innerHTML += '<br> reprovado!'
+        }
+    }
+
+
+
 }
