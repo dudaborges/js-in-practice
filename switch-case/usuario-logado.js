50862ab7da1aa86ddf9477d9297a87d199430b9f
dudaborges <duda.pborges92@gmail.com>
2022-10-13 21:50:07 -0300

    added: switch case

---
+++ switch-case/usuario-logado.js
@@ -0,0 +1,10 @@
+let autenticado = true
+
+switch(autenticado){
+    case true:
+        console.log('Usuário logado')
+        break
+    case false:
+        console.log('Usuário não autenticado')
+        break    
+}
